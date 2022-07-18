f9c03b388a7277dd4f8bbc954d512fb108d3b28f
EvDenny <107444879+EvDenny@users.noreply.github.com>
2022-07-18 04:47:55 +0000

    Author:    EvDenny <107444879+EvDenny@users.noreply.github.com>
     On branch main
     Your branch is ahead of 'origin/main' by 3 commits.
       (use "git push" to publish your local commits)
    
     Changes to be committed:
    	new file:   file
    	new file:   file.cpp
    	modified:   main.code-workspace

---
+++ file.cpp
@@ -0,0 +1,32 @@
+//IDEA: Create a class that stores information into a filesystem and accesses said information in a way that is password protected.
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cstdlib>
+#include <ctime>
+#include <cmath>
+#include <thread>
+#include <chrono>
+#include <bits/stdc++.h>
+#include <sstream>
+#include <iomanip>
+#include <cstdio>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <array>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <list>
+#include <forward_list>
+#include <iterator>
+#include <utility>
+
+using namespace std;
+using namespace std::chrono;
+using namespace std::this_thread;
+
